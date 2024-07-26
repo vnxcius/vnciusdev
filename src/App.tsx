e38a11dd02863d87bbb9fe1681c0e1766c22fab4
Vini <vsgh-gouveia@hotmail.com>
2024-07-26 02:44:55 -0300

    feat: initial hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,47 @@
 import { Link } from "react-router-dom";
 import LanguageSelector from "./components/languague-selector";
+import { t } from "i18next";
+import Button from "./components/button";
 
 function App() {
 
   return (
     <>
-      <header className="py-3">
-        <nav className="max-w-xl flex mx-auto justify-between items-center">
-          <Link to="/" className="text-xl font-medium tracking-wider">vncius</Link>
-          <ul>
+      <header className="py-5">
+        <nav className="max-w-5xl flex mx-auto justify-between items-center">
+          <Link to="/" className="text-2xl font-playwrite">vncius</Link>
+          <ul className="relative">
             <li>
               <LanguageSelector />
             </li>
           </ul>
         </nav>
       </header>
-      <div></div>
+      <section className="grid grid-cols-2 relative max-w-5xl my-14 mx-auto text-neutral-800">
+        <div className="sticky top-0 flex flex-col place-self-start items-start gap-10">
+          <img src="/img/handsome.webp" alt="Vinícius Simon" className="rounded-[36px]" width={190} height={'auto'} />
+          <div className="space-y-2">
+            <p className="font-merriweather text-3xl">
+              {t('hi')} Vinícius Simon
+            </p>
+            <p className="text-2xl">
+              {t('role')}
+            </p>
+          </div>
+        </div>
+        <div className="space-y-10">
+          <p className="font-merriweather text-2xl">
+            {t('description')}
+          </p>
+
+          <div className="space-x-4 pb-16">
+            <Button content={t('contact')} />
+            <Button content={t('myWork')} variant="secondary" />
+          </div>
+
+          <hr className="border-neutral-200" />
+        </div>
+      </section>
     </>
   )
 }

1e6ef317c4fc017177d4b507e0a690889738d94b
Vini <vsgh-gouveia@hotmail.com>
2024-07-26 02:44:04 -0300

    feat: create button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,24 @@
 
 type Props = {
+  id?: string,
   content: string,
+  variant?: string
   onClick?: () => void
 }
 
 const Button = (props: Props) => {
+  const variants: any = {
+    base: " px-5 py-2 text-center rounded-full duration-300 hover:brightness-95 hover:cursor-pointer",
+    primary: "bg-neutral-800 text-primary hover:bg-neutral-700",
+    secondary: "bg-primary text-neutral-800 border border-neutral-300",
+  }
+
   return (
-    <button onClick={props.onClick}>
-      {props.content}
+    <button className={props.variant ?
+      variants[props.variant] + variants["base"] : variants["primary"] + variants["base"]}
+      onClick={props.onClick}
+      id={props.id}>
+      <span>{props.content}</span>
     </button>
   )
 }

81e96797f252c1dde391e3f7b9f985a02208c408
Vini <vsgh-gouveia@hotmail.com>
2024-07-27 01:41:55 -0300

    chore: update button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,23 +1,29 @@
 
 type Props = {
   id?: string,
+  icon?: JSX.Element,
+  className?: string,
   content: string,
-  variant?: string
+  variant?: "primary" | "secondary" | "tertiary",
   onClick?: () => void
 }
 
 const Button = (props: Props) => {
   const variants: any = {
-    base: " px-5 py-2 text-center rounded-full duration-300 hover:brightness-95 hover:cursor-pointer",
-    primary: "bg-neutral-800 text-primary hover:bg-neutral-700",
-    secondary: "bg-primary text-neutral-800 border border-neutral-300",
+    base: props.className + " px-5 py-2 text-center inline-flex items-center gap-2 rounded-full duration-300 hover:brightness-95 hover:cursor-pointer",
+    primary: "bg-neutral-800 text-primary hover:bg-neutral-700 dark:text-neutral-200 ",
+    secondary: "bg-primary text-neutral-800 border border-neutral-300 dark:bg-neutral-925 dark:text-neutral-300 dark:border-neutral-700 ",
+    tertiary: "bg-neutral-50 border border-neutral-300 text-neutral-800 hover:cursor-default ",
   }
 
   return (
-    <button className={props.variant ?
-      variants[props.variant] + variants["base"] : variants["primary"] + variants["base"]}
+    <button
+      id={props.id}
       onClick={props.onClick}
-      id={props.id}>
+      className={
+        props.variant ?
+          variants[props.variant] + variants["base"] : variants["primary"] + variants["base"]}>
+      {props.icon}
       <span>{props.content}</span>
     </button>
   )

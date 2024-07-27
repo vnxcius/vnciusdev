950e475ca12cf20e05a08d32a0e73899302f5636
Vini <vsgh-gouveia@hotmail.com>
2024-07-27 01:42:52 -0300

    chore: create icon components

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,39 @@
+import ReactIcon from "./components/react";
+import Docker from "./components/docker";
+import Nextjs from "./components/nextjs";
+import Go from "./components/go";
+import PostgreSQL from "./components/postgres";
+import TailwindCSS from "./components/tailwind";
+import MySQL from "./components/mysql";
+import TypeScript from "./components/typescript";
+import Firebase from "./components/firebase";
+import Figma from "./components/figma";
+import Laravel from "./components/laravel";
+import Php from "./components/php";
+import Java from "./components/java";
+import JavaScript from "./components/javascript";
+import LinkedIn from "./components/linkedin";
+import Github from "./components/github";
+import Cobol from "./components/cobol";
+import Discord from "./components/discord";
+
+export {
+  ReactIcon,
+  Docker,
+  Nextjs,
+  Go,
+  PostgreSQL,
+  TailwindCSS,
+  MySQL,
+  TypeScript,
+  Firebase,
+  Figma,
+  Laravel,
+  Php,
+  Java,
+  JavaScript,
+  LinkedIn,
+  Github,
+  Cobol,
+  Discord
+}
